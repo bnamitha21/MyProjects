6bab84ced2d9b1a8642d18a194415f7e2566ae67
bnamitha21 <namithabajjuri21@gmail.com>
2025-12-23 18:58:38 +0530

    Delete backend directory

--- backend/utils/roleUtils.js
+++
@@ -1,8 +0,0 @@
-export const normalizeRole = (role = '') => (
-  role === 'worker' ? 'employee' : role
-);
-
-export const EMPLOYEE_ROLE_FILTER = ['employee', 'worker'];
-
-export const isEmployeeRole = (role = '') => normalizeRole(role) === 'employee';
-
